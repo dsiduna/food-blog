a2715f734661f28ed47412d61ca3bcd7b36b6ea4
Denis Siduna <125746473+munaashe@users.noreply.github.com>
2023-10-15 23:15:56 +0200

    code cleanup

---
+++ app/layout.jsx
@@ -0,0 +1,17 @@
+import './globals.css'
+import { Montserrat } from 'next/font/google'
+
+const montserrat = Montserrat({ subsets: ['latin'] })
+
+export const metadata = {
+  title: 'Team Fullo',
+  description: 'The food people',
+}
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body className={montserrat.className}>{children}</body>
+    </html>
+  )
+}
---
+++ app/page.jsx
@@ -0,0 +1,8 @@
+
+export default function Home() {
+  return (
+    <main className="">
+      <h3>here we go again</h3>
+    </main>
+  )
+}
